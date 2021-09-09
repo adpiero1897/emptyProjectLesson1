fe1b760003ffbb1273a506a53236437dddbc3b5f
the_Trolling_stone <adpiero1897@mail.ru>
2021-09-09 18:12:26 +0300

    My homework is ready

---
+++ src/main/java/HomeworkL1/HomeWorkApp.java
@@ -0,0 +1,66 @@
+package HomeworkL1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args)
+    {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords()
+    {
+        System.out.print("Orange\nBanana\nApple\n");
+    }
+
+    public static void checkSumSign()
+    {
+        int a = 666;
+        int b = -100500;
+        if(a+b >= 0)
+        {
+            System.out.println("Сумма положительна");
+        }
+        else
+        {
+            System.out.println("Сумма отрицательна");
+        }
+    }
+
+    public static void printColor()
+    {
+        int value = 68;
+
+        if(value <=0)
+        {
+            System.out.println("Красный");
+        }
+        else if(value <= 100)
+        {
+            System.out.println("Желтый");
+        }
+        else
+        {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers()
+    {
+        int a = 263282;
+        int b = 93268;
+
+        if(a >= b)
+        {
+            System.out.println("a>=b");
+            System.out.println(a+">="+b); // Добавляю ко всему вывод значений переменных, участвующих в сравнении
+        }
+        else
+        {
+            System.out.println("a<b");
+            System.out.println(a+"<"+b); // Добавляю ко всему вывод значений переменных, участвующих в сравнении
+        }
+    }
+}
